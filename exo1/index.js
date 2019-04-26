e0654136e6c7c44a4607ea54f404119c36f6a88d
Tom <thomas.bozon@campu_numerique.fr>
2019-04-26 14:43:13 +0200

    exo1 vue js upgrade

--- exo1/index.js
+++ exo1/index.js
@@ -2,8 +2,12 @@
 var app = new Vue({
   el: '#app',
   data: {
+
+    
+    title:'hello',
     value_test: '',
     value_test1:'',
+    value_test2:"",
     show: false,
     showpic:false,
     active: false,
